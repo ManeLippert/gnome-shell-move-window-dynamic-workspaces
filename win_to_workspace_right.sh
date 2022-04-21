53a09d9a654b87963a048e215f56fcc2f69f7262
ManeLippert <manuel-lippert@outlook.de>
2022-04-21 20:25:32 +0200

    Update rightward script

--- win_to_workspace_right.sh
+++ win_to_workspace_right.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# xdotool and wmctrl needed
+# xdotool and wmctl needed
 
 # generate desktop number for the rightward monitor
 DESKTOPRIGHTWARD="$(($(xdotool getactivewindow get_desktop_for_window)+1))"
+DESKTOPEND="$(($(xdotool get_num_desktops)-1))"
 DESKTOPSTART=0
 
 # count open windows to know the end of the workspaces
-OPENWINDOWS=$(wmctrl -l | cut -d ' ' -f3 | grep $(wmctrl -d | grep '*' | cut -d ' ' -f1) | wc -l)
+LASTWORKSPACE="$(($(xdotool get_num_desktops)-2))"
+CURRENTWORKSPACE=$(wmctrl -d | grep '*' | cut -d ' ' -f1)
+OPENWINDOWS=$(wmctrl -l | cut -d ' ' -f3 | grep $CURRENTWORKSPACE | wc -l)
 
 # change desktop to the right
-if [[ $OPENWINDOWS -eq 1 ]]
+if [[ $OPENWINDOWS -eq 1 ]] && [[ $CURRENTWORKSPACE -eq $LASTWORKSPACE ]]
 then
     xdotool getactivewindow set_desktop_for_window $DESKTOPSTART && xdotool set_desktop $DESKTOPSTART
 else
